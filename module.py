3ebe5d812fe8fc9250f4e0427628a66686a6b6f2
Benjamin Feder <benfeder@Benjamins-MacBook-Air.local>
2019-12-11 13:21:10 -0500

    updated and cleanedmodule

---
+++ module.py
@@ -0,0 +1,8 @@
+# Define function
+
+def world():
+    print("Hello, world!!!")
+
+
+def add_8(x):
+    print(x + 8)
---
+++ sample_script.py
@@ -0,0 +1,8 @@
+# Import classes from your brand new package
+import module
+
+#call function
+module.world()
+
+#call add_8 function
+module.add_8(2)

7b30d744f3b2f15930fd5ad616068d7a29d8457a
Benjamin Feder <benfeder@Benjamins-MacBook-Air.local>
2019-12-11 14:46:01 -0500

    added dummy_counts function

--- module.py
+++ module.py
@@ -6,3 +6,19 @@ def world():
 
 def add_8(x):
     print(x + 8)
+
+
+def dummy_counts(df):
+    # only get columns that are dummied
+    dummy = [c for c in df.columns if df[c].dtype == 'uint8']
+
+    # check if all counts are at least 10
+    less_10 = [var for var in dummy if df[var].value_counts()[0] < 10 or df[var].value_counts()[1] < 10]
+
+    #if there are none that don't satisfy condition print all the counts, else print the offenders
+    if len(less_10) > 0:
+        print("These dummy variables didn't have counts of at least 10 for both when they are 1 or 0: \n")
+        return([print(var) for var in less_10])
+    else:
+    # get the value counts for each dummy
+        return([print(df[var].value_counts()) for var in dummy])
--- sample_script.py
+++ sample_script.py
@@ -1,8 +1,21 @@
 # Import classes from your brand new package
 import module
+import pandas as pd
 
 #call function
 module.world()
 
 #call add_8 function
 module.add_8(2)
+
+# test dummy_counts function
+# get a series
+s = pd.Series(list('abcaaaaaaaaaaaaaaaaabbbbbbbbbbbbbb'))
+# make dummies of the series
+d = pd.get_dummies(s)
+
+#call function when it should give list of columns that don't pass test
+module.dummy_counts(d)
+
+#columns that should pass test
+module.dummy_counts(d[['a','b']])

441cc855c703a14eaa1fb24a238936aaf1ecd77a
Benjamin Feder <benfeder@Benjamins-MacBook-Air.local>
2019-12-11 15:03:58 -0500

    added quick note in sample script about why printing new lines

--- sample_script.py
+++ sample_script.py
@@ -2,11 +2,17 @@
 import module
 import pandas as pd
 
+
+## NOTE: Included print("\n") under each function call so it's easy to designate
+## between outputs
+
 #call function
 module.world()
+print("\n")
 
 #call add_8 function
 module.add_8(2)
+print("\n")
 
 # test dummy_counts function
 # get a series
@@ -16,6 +22,8 @@ d = pd.get_dummies(s)
 
 #call function when it should give list of columns that don't pass test
 module.dummy_counts(d)
+print("\n")
 
 #columns that should pass test
 module.dummy_counts(d[['a','b']])
+print("\n")

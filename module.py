00fd797b64e498fdcab61823e0344136876a468c
Benjamin Feder <benfeder@Benjamins-MacBook-Air.local>
2020-03-06 16:44:20 -0500

    added disclosure review guidelines and some boxplot stuff

--- module.py
+++ module.py
@@ -1,5 +1,6 @@
-# requires pandas
+# requires a few packages
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 # finds counts for dummy variables and either gives counts if all satisfy or
@@ -37,3 +38,37 @@ def fuzzy_crosstab(col1,col2):
         if xtab_fixed[i].dtype == 'int64':
             xtab_fixed.loc[xtab_fixed[i] < 10, i] = 'NaN'
     return xtab_fixed
+
+#Creating safe boxplot for one statistic
+def safe_boxplot(df, stat, positions=None, widths=None, vert=True, patch_artist=False, shownotches=False, 
+                 showmeans=False, showcaps=True, showbox=True, showfliers=False, boxprops=None, whiskerprops=None, 
+                 flierprops = None, medianprops=None, capprops=None, meanprops=None, meanline=False, 
+                 manage_ticks=True, zorder=None):
+    
+    # find fuzzy 25, 50, and 75 quartiles
+    fuzzy_med = (df[stat].quantile(.45) + df[stat].quantile(.55))/2
+    fuzzy_25 = (df[stat].quantile(.20) + df[stat].quantile(.30))/2
+    fuzzy_75 = (df[stat].quantile(.70) + df[stat].quantile(.80))/2
+    
+    # find outlier cutoffs for mins and maxs
+    outlier_min = fuzzy_25 - 1.5*(fuzzy_75-fuzzy_25)
+    outlier_max = fuzzy_75 + 1.5*(fuzzy_75-fuzzy_25)
+    
+    # eliminate all outliers
+    df_nonoutliers = df[(df[stat] > outlier_min) & (df[stat] < outlier_max)]
+    
+    # find min and max in non-outliers
+    fuzzy_min = (sorted(df_nonoutliers[stat])[0] + sorted(df_nonoutliers[stat])[1])/2
+    fuzzy_max = (sorted(df_nonoutliers[stat])[-1] + sorted(df_nonoutliers[stat])[-2])/2
+    
+    #combine stats into a boxplot
+    stats = [
+        {'med': fuzzy_med, 'q1': fuzzy_25, 'q3': fuzzy_75, 'whislo': fuzzy_min, 'whishi': fuzzy_max}
+    ]
+    
+    _, ax = plt.subplots()
+    
+    #return plot
+    return(ax.bxp(stats, positions, widths, vert, patch_artist, shownotches, showmeans, 
+                  showcaps, showbox, showfliers, boxprops, whiskerprops, flierprops, medianprops, capprops, 
+                  meanprops, meanline, manage_ticks, zorder))
